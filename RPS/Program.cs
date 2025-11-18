72a6c4f58447f85af9f5c784f052f034d5b90677
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-11-18 10:19:19 +0000

    Check sign correctness for both players

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -1,13 +1,13 @@
 ﻿List<string> allowedSigns = ["rock", "paper", "scissors"];
-string GetCorectSign()
+string GetCorrectSign(string playerName)
 {
-    Console.WriteLine($"Player 1, choose your sign ({string.Join('/', allowedSigns)})");
+    Console.WriteLine($"{playerName}, choose your sign ({string.Join('/', allowedSigns)})");
     string sign = Console.ReadLine()!;
 
     // while (!(firstSign.Equals(allowedSigns[0], stringComparison) || firstSign.Equals(allowedSigns[1], stringComparison) || firstSign.Equals(allowedSigns[2], stringComparison)))
     while (!allowedSigns.Contains(sign, StringComparer.OrdinalIgnoreCase))
     {
-        Console.WriteLine($"Player 1, choose correct sign ({string.Join('/', allowedSigns)})");
+        Console.WriteLine($"{playerName}, choose correct sign ({string.Join('/', allowedSigns)})");
         sign = Console.ReadLine()!;
     }
     return sign;
@@ -29,11 +29,8 @@ Console.WriteLine("Let's play Rock-Paper-Scissors!");
 //     Console.WriteLine($"Player 1, choose correct sign ({string.Join('/', allowedSigns)})");
 //     firstSign = Console.ReadLine()!;
 // }
-string firstSign = GetCorectSign();
-
-
-Console.WriteLine($"Player 2, choose your sign ({string.Join('/', allowedSigns)})");
-string secondSign = Console.ReadLine()!;
+string firstSign = GetCorrectSign("Player 1");
+string secondSign = GetCorrectSign("Player 2");
 
 
 if (firstSign.Equals(secondSign, stringComparison))
