8b084c065518d4017c81af22ff8c9cbdd07070c8
bober <bober@co>
2025-12-02 11:26:15 +0100

    loo[ game and count points

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -4,7 +4,6 @@ string GetCorrectSign(string playerName)
     Console.WriteLine($"{playerName}, choose your sign ({string.Join('/', allowedSigns)})");
     string sign = Console.ReadLine()!;
 
-   
     while (!allowedSigns.Contains(sign, StringComparer.OrdinalIgnoreCase))
     {
         Console.WriteLine($"{playerName}, choose correct sign ({string.Join('/', allowedSigns)})");
@@ -13,32 +12,48 @@ string GetCorrectSign(string playerName)
     return sign;
 }
 
+string GetCorrectRandomSign(string playerName)
+{
+    int signIndex = Random.Shared.Next(allowedSigns.Count);
+    string sign = allowedSigns[signIndex];
+    Console.WriteLine($"{playerName} selected {sign}");
+
+    return sign;
+}
 
 const StringComparison stringComparison = StringComparison.OrdinalIgnoreCase;
-
-Console.WriteLine("Let's play Rock-Paper-Scissors!");
-
-
-string firstSign = GetCorrectSign("Player 1");
-string secondSign = GetCorrectSign("Player 2");
-
-// 1. Pobierz indeks znaku podanego przez osobę drugą (np. 0, 1, 2) - nazwę to secondSignIndex
-int secondSignIndex = allowedSigns.IndexOf(secondSign);
-// 2. Wylicz indeks znaku, który wygrywa z podanym przez osobę drugą - wzór - (secondSignIndex + 1) % rozmiarListy
-int winningSignIndex = (secondSignIndex + 1) % allowedSigns.Count;
-// 3. Czy indeks znaku podanego przez pierwszą osobę to indeks wyliczony w punkcie 2.
-int firstSignIndex = allowedSigns.IndexOf(firstSign);
-
-
-if (firstSign.Equals(secondSign, stringComparison))
+int firstplayerpoints=0;
+int secondplayerpoints=0;
+while(true)
 {
-    Console.WriteLine("It's a draw!");
-}
-else if (firstSignIndex == winningSignIndex)
-{
-    Console.WriteLine("First player won!");
-}
-else
-{
-    Console.WriteLine("Second player won!");
+    Console.WriteLine("Let's play Rock-Paper-Scissors!");
+
+    string firstSign = GetCorrectSign("Player 1");
+    string secondSign = GetCorrectRandomSign("Player 2");
+
+    // 1. Pobierz indeks znaku podanego przez osobę drugą (np. 0, 1, 2) - nazwę to secondSignIndex
+    int secondSignIndex = allowedSigns.IndexOf(secondSign);
+    // 2. Wylicz indeks znaku, który wygrywa z podanym przez osobę drugą - wzór - (secondSignIndex + 1) % rozmiarListy
+    int winningSignIndex = (secondSignIndex + 1) % allowedSigns.Count;
+    // 3. Czy indeks znaku podanego przez pierwszą osobę to indeks wyliczony w punkcie 2.
+    int firstSignIndex = allowedSigns.IndexOf(firstSign);
+
+
+    if (firstSign.Equals(secondSign, stringComparison))
+    {
+        Console.WriteLine("It's a draw!");
+    }
+    else if (firstSignIndex == winningSignIndex)
+    {
+        Console.WriteLine("First player won!");
+        firstplayerpoints =+ 1 ;
+    }
+    else
+    {
+        Console.WriteLine("Second player won!");
+        secondplayerpoints =+ 1;
+    }
+    Console.WriteLine($"First player:{firstplayerpoints}");
+    Console.WriteLine($"Second Player:{secondplayerpoints}");
+
 }
